52c8bcd3196d47a6b4fcc081b1629ff93f43857e
Emanuelortega <matias_ortega1996@outlook.com>
2022-06-22 19:22:52 -0300

    CLASE 41
    CLASE 2 HTML

---
+++ app-web/src/main/java/ar/com/educacionit/web/controllers/AltaFormularioSerlet.java
@@ -0,0 +1,28 @@
+package ar.com.educacionit.web.controllers;
+
+public class AltaFormularioSerlet /*extends algo*/ {
+
+/*	//GET
+	public void get(request, response) {
+
+		//capturar los datos que se enviaron desde el form
+		
+		//aplica logica
+		
+		//responder algo
+	}
+	
+	
+	//POST
+	//FIlE
+	public void post (request, response) {
+
+		//capturar los datos que se enviaron desde el form
+		
+		//aplica logica
+		
+		//responder algo
+	}
+*/
+	
+}
