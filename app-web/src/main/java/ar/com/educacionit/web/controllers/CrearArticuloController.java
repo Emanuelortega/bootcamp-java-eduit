2a8a535fcf84b0d6362990651a39460a0b464011
Emanuelortega <matias_ortega1996@outlook.com>
2022-07-29 20:23:08 -0300

    clase 55
    *eliminar producto
    *alta producto con mensajeria
    *editar producto

--- app-web/src/main/java/ar/com/educacionit/web/controllers/CrearArticuloController.java
+++ app-web/src/main/java/ar/com/educacionit/web/controllers/CrearArticuloController.java
@@ -2,6 +2,8 @@ package ar.com.educacionit.web.controllers;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -24,24 +26,33 @@ public class CrearArticuloController extends BaseServlet{
 		String titulo = req.getParameter("titulo");
 		String codigo = req.getParameter("codigo");
 		String stock = req.getParameter("stock");
-		String categoriaId = req.getParameter("categoria");//1
-		String marcasId = req.getParameter("marcas");//1
+		String categoriaId = req.getParameter("categoriaId");//1
+		String marcasId = req.getParameter("marcaId");//1
 		
-		ViewsEnum target = ViewsEnum.LISTADO;
+		//valido previamente
+		//IValidador validator = new AltaWebValidator(req);//request es objeto de la web.
+		
+		ViewsEnum target = ViewsEnum.LISTADO_CONTROLLER;
+		
+		ArticulosService articuloService = new ArticulosServiceImpl();
 		
 		try {
-			ArticulosService articuloService = new ArticulosServiceImpl();
+			
+			//IValidador validator2 = new AltaValidator(precio,titulo,....);//request es objeto de la web.
 
 			Articulo nuevo = new Articulo(titulo, codigo, null, Double.parseDouble(precio), Long.parseLong(stock), Long.parseLong(marcasId), Long.parseLong(categoriaId));
 			
 			articuloService.create(nuevo);
-			
 			//msj ok
 			super.setAttributes(AttributesEnum.EXITO, req, "Producto id"+nuevo.getId()+" OK");
 		} catch (ServiceException | SQLException e) {
-			super.setAttributes(AttributesEnum.ERROR_GENERAL, req, e.getMessage());
+			super.setAttributes(AttributesEnum.ERROR_GENERAL, req, e.getMessage() + "-" + e.getCause().getMessage());
 			//msj error
-		}
+		} /*catch (ValidatoException ve) {
+			super.setAttribute(AttributesEnum.ERROR_GENERAL, req, e.getMessage());
+		}*/
+		//de un controller voy a otro controller
+		//getServletContext().getRequestDispatcher("/controller/ListadoController").forward(req, resp);
 		
 		redirect(target, req, resp);
 	}
