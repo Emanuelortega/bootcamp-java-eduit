72c4ea7833edf74edd86a39026cef032b497bbaf
Emanuelortega <matias_ortega1996@outlook.com>
2022-09-18 22:20:09 -0300

    Clase 68
     Exportar PDF Action

--- app-web/src/main/java/ar/com/educacionit/web/enums/PDFAction.java
+++ app-web/src/main/java/ar/com/educacionit/web/enums/PDFAction.java
@@ -1,12 +1,15 @@
 package ar.com.educacionit.web.enums;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-import ar.com.educacionit.domain.Articulo;
-
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 public class PDFAction implements IExportable {
 
 	@Override
@@ -15,15 +18,29 @@ public class PDFAction implements IExportable {
 		//obtener el jasper
 		
 		//armar los datos que son parametros
-		
-		Map<String , String> params = new HashMap<>();
-		params.put("USERNAME","EMANUEL");
+		Map<String,Object> params = new HashMap<>();
+		params.put("USERNAME", "EMANUEL");
 		
 		//armar los datos de la lista > field
-		
-		//llenar el jasper
-		
-		return "";
-	}
-}
+		String fileName;
+		try {
+			String pathFile = "/reports/articulos-error.jasper";
+			
+			fileName = this.getClass().getResource(pathFile).toURI().getPath();
+			
+			FileInputStream fis = new FileInputStream(new File(fileName));
+			JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(datos);
 
+			JasperPrint jasperPrint = JasperFillManager.fillReport(fis,params,beanColDataSource);
+			
+			System.out.println(jasperPrint.getName());
+			//llenar el jasper
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		return ""; 
+	}
+
+}
